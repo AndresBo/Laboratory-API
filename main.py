fc0a3a3f82c080335b7ee55a47a2b6afd44c926b
AndresBo <anndresfborrero@gmail.com>
2023-03-06 11:53:48 +1000

    added bcrypt and JWT objects to main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 
 db= SQLAlchemy()
 ma= Marshmallow()
+bcrypt = Bcrypt()
+jwt = JWTManager()
 
 
 def create_app():
@@ -13,6 +17,8 @@ def create_app():
 
     db.init_app(app)
     ma.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
 
     # import controllers and activate Blueprints:
     from commands import db_commands
