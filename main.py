e776f6079adc6752895579aa8b02c9c46d78c3c7
AndresBo <anndresfborrero@gmail.com>
2023-03-06 11:18:37 +1000

    added marshmallow to main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 db= SQLAlchemy()
+ma= Marshmallow()
 
 
 def create_app():
@@ -10,9 +12,9 @@ def create_app():
     app.config.from_object("config.app_config")
 
     db.init_app(app)
+    ma.init_app(app)
     
-    
-
+    # import controllers and activate Blueprints:
     from controllers import registerable_controllers
 
     for controller in registerable_controllers:
