3d4573ec0f3bd453b3e3735bf4cf543c233c953e
AndresBo <anndresfborrero@gmail.com>
2023-03-06 13:16:48 +1000

    completed analyst schema

--- schemas/analyst_schema.py
+++ schemas/analyst_schema.py
@@ -6,3 +6,9 @@ from models.analysts import Analyst
 class AnalystSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Analyst
+
+    password = ma.String(validate=Length(min=6))
+
+
+analyst_schema = AnalystSchema()
+analysts_schema = AnalystSchema(many=True)
