42527fe4e85520775bd2cc8280313b9dc0b0a5ac
AndresBo <anndresfborrero@gmail.com>
2023-03-06 13:28:06 +1000

    added get_analysts method

--- controllers/analysts_controller.py
+++ controllers/analysts_controller.py
@@ -1,4 +1,19 @@
 from flask import Blueprint, jsonify, request, abort
 from main import db
 from models.analysts import Analyst
+from schemas.analyst_schema import analyst_schema, analysts_schema
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
+
+analysts = Blueprint('analysts', __name__, url_prefix="/analysts")
+
+
+@analysts.route("/", method=["GET"])
+def get_analysts():
+    analysts_list = Analyst.query.all()
+
+    result = analysts_schema.dump(analysts_list)
+
+    return jsonify(result)
+
 
