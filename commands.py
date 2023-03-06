e159729d445214a335aaf768e35830c44799d489
AndresBo <anndresfborrero@gmail.com>
2023-03-06 11:29:51 +1000

    create_db and drop_db methods added

---
+++ commands.py
@@ -0,0 +1,19 @@
+from main import db
+from flask import Blueprint
+# bcyrpt
+from models.analyser import Analyser
+
+
+db_commands = Blueprint("db", __name__)
+
+
+@db_commands.cli.command("create")
+def create_db():
+    db.create_all()
+    print("Tables created")
+
+
+@db_commands.cli.command("drop")
+def drop_db():
+    db.drop_all()
+    print("Tables dropped")
