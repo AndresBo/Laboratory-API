ea1bfc09fba9f86112fb0c049a963cde465442bd
AndresBo <anndresfborrero@gmail.com>
2023-03-08 11:53:33 +1000

    uncommented Test model

--- models/tests.py
+++ models/tests.py
@@ -1,11 +1,11 @@
-# from main import db
+from main import db
 
 
-# class Test(db.Model):
-#     __tablename__= "tests"
+class Test(db.Model):
+    __tablename__= "tests"
 
-#     id = db.Column(db.Integer(), primary_key=True)
-#     name = db.Column(db.String(), nullable=False, unique=True)
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(), nullable=False, unique=True)
 #     # requests_tests = db.relationship(
 #     #     "Request_test",
 #     #     backref="test",
