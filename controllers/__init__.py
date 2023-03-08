113f4489781c7954b836e06f0a453d15c6dac754
AndresBo <anndresfborrero@gmail.com>
2023-03-08 12:14:59 +1000

    added tests controller to __init__py so it can accessed from main.py

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,9 +1,11 @@
 from controllers.analysers_controller import analysers
 from controllers.analysts_controller import analysts
+from controllers.tests_controller import tests
 
 
 
 registerable_controllers = [
     analysers,
     analysts,
+    tests
 ]
