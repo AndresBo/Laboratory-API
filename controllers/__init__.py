1c39d0b80dbaa470a786ff4c608294eb744b63ef
AndresBo <anndresfborrero@gmail.com>
2023-03-12 20:59:45 +1000

    added requests blueprint to __init__.py

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,11 +1,13 @@
 from controllers.analysers_controller import analysers
 from controllers.analysts_controller import analysts
 from controllers.tests_controller import tests
+from controllers.requests_controller import requests
 
 
 
 registerable_controllers = [
     analysers,
     analysts,
-    tests
+    tests,
+    requests
 ]
