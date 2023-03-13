41567990193e8aaa998a213d815263028a1568e4
AndresBo <anndresfborrero@gmail.com>
2023-03-13 12:05:42 +1000

    added analyst_email foreign key to request schema

--- schemas/request_schema.py
+++ schemas/request_schema.py
@@ -3,7 +3,7 @@ from main import ma
 
 class RequestSchema(ma.Schema):
     class Meta:
-        fields = ("id", "date", "status")
+        fields = ("id", "date", "status", "analyst_email")
 
 request_schema = RequestSchema()
 
