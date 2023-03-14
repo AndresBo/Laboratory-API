f9d4c1987d6633d06981db44ae0baea17e63ad7b
AndresBo <anndresfborrero@gmail.com>
2023-03-14 11:10:41 +1000

    added two-way many-to-many relationship to Request model

--- models/requests.py
+++ models/requests.py
@@ -9,8 +9,8 @@ class Request(db.Model):
     status = db.Column(db.String())
     analyst_email = db.Column(db.String, db.ForeignKey("analysts.email"), nullable=False)
     analyser_name = db.Column(db.String, db.ForeignKey("analysers.name"), nullable=False)
-    # requests_tests = db.relationship(
-    #     "Request_test",
-    #     backref="request",
-    #     cascade="all, delete"
-    # )
+    test = db.relationship(
+        "Request_test",
+        backref="request",
+        cascade="all, delete"
+    )
