5635c8b47576d1b04774c0c73d0cb22c862e4381
AndresBo <anndresfborrero@gmail.com>
2023-03-18 16:11:13 +1000

    typos

--- models/requests_tests.py
+++ models/requests_tests.py
@@ -5,7 +5,7 @@ class Request_test(db.Model):
     __tablename__= "requests_tests"
 
     id = db.Column(db.Integer, primary_key=True)
-    # two foreing keys for many-to-many relationship: one request can have many tests,
+    # two foreign keys for many-to-many relationship: one request can have many tests,
     # one test can belong to many requests.
     request_id = db.Column(db.Integer, db.ForeignKey("requests.id"), nullable=False)
     test_name = db.Column(db.String, db.ForeignKey("tests.name"), nullable=False)
